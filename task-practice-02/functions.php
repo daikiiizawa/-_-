14b0e66bc65503dbec57ed5a3b48dc878fa71876
daikiiizawa <daiki.813.gon@gmail.com>
2016-08-12 09:34:52 +0000

    task-practice-02の内容追加

---
+++ task-practice-02/functions.php
@@ -0,0 +1,25 @@
+<!-- task07完成版 -->
+<?php
+// DB接続
+function connectDatabase()
+{
+  try
+  {
+    return new PDO(DSN,USER,PASSWORD);
+  }
+  catch(PDOException $e)
+  {
+    echo $e->getMessage();
+    exit;
+  }
+}
+
+
+//エスケープ処置
+function h($s)
+{
+  return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
+}
+
+
+
